620a5f025033c404d00c6266e4ea7a9bae3599d8
ThiruchelvanTamilselvan <tthiruchelvansibi@gmail.com>
2025-05-16 22:50:54 +0530

    HeroStones add

--- src/DepartmentsData.js
+++ src/DepartmentsData.js
@@ -7,28 +7,28 @@ export const departmentsData = {
     cards: [
       {
         title: "Origins of Hero Stones",
-        description: "Explores early forms of hero stones in South India...",
-        image: "/images/hero1.jpg",
+        description: "It is painted and worshipped by the villagers. It is carved with a scene of a warrior fighting a tiger with a sword and bow. It is carved as if he is stabbing the tiger in the mouth with a dagger.His two hunting dogs are fighting with the tiger. The ornaments are visible. This center stone was taken because he fought with the tiger and killed it and died himself.His two wives are also related to him. Therefore, it can be called the Tiger-slayer Stone or the Sati-kal. Parrots are shown to symbolize the prosperity of the area.",
+        image: "/images/HeroStones/1.jpg",
       },
       {
         title: "Symbolism and Inscriptions",
-        description: "Analyzes the symbols and texts found on these stones...",
-        image: "/images/hero2.jpg",
+        description: "This nadugala reminds us that a woman is always a symbol of sacrifice. The nadugala in the Kundani kingdom. This is found on a small nadugala with an inscription. In it, it is mentioned as the beheading of the head of the goddess.She, who is in the form of a heroine, sacrificed herself for some good cause. After this, a large center stone was made in the same form and is still worshipped as Sakyammal. This is 400 years old.",
+        image: "/images/HeroStones/2.jpg",
       },
       {
         title: "Historical Context of Hero Stones",
-        description: "Explores the role of hero stones in historical narratives...",
-        image: "/images/hero3.jpg",
+        description: "We are the owners of the climbing stone. Our research team has discovered this rare climbing stone in the coconut grove of Agaram, a village along the banks of the Thenpennai River. This stone is about 300 years old.The bull is shown running with its front legs raised to the side. Behind it, the warrior, trying to tame the bull, holds the bull's rump tightly and hangs with his feet clasped in the bull's front legs.This causes the bull's tongue to hang out of its mouth. This central stone sculpture is carved in accordance with the word climbing.",
+        image: "/images/HeroStones/3.jpg",
       },
       {
         title: "Material and Construction",
-        description: "Examines the materials used in constructing hero stones...",
-        image: "/images/hero4.jpg",
+        description: "The symbol of bravery is the tombstone collection taken from the Thokarapalli Sanarappan Temple to commemorate the warriors who fought against Adhiyama and protected their village.The inscriptions here indicate that Perakadai Ekambaran, Kumberakadai, and Velmutharaiyan Sevakar Manian died bravely. The Nadugal puja is still held on the 27th of Margazhi, the day the battle took place.",
+        image: "/images/HeroStones/4.jpg",
       },
       {
         title: "Cultural Significance",
-        description: "Highlights the cultural significance of hero stones...",
-        image: "/images/hero5.jpg",
+        description: "There is nothing that cannot be conquered by music. The center stone of these two tambourine playing women, who have conquered the Penneyanandavar and the devotees with their music, has been taken and placed near the temple gate of the Penneswara Math.",
+        image: "/images/HeroStones/5.jpg",
       },
     ]
   },
@@ -37,28 +37,28 @@ export const departmentsData = {
     cards: [
       {
         title: "நடுகற்களின் தோற்றம்",
-        description: "தென் இந்தியாவில் நடுகற்களின் ஆரம்ப வடிவங்களை ஆராய்கிறது...",
-        image: "/images/hero1.jpg",
+        description: "கிராம மக்களால் வண்ணம் தீட்டப்பட்டு வழிபாட்டில் உள்ளது. வீரன் வாள் மற்றும் வில்லோடு புலியோடு போராடும் காட்சி செதுக்கப்பட்டுள்ளது. கட்டாரியை புலியின் வாயில் குத்துவது போல் செதுக்கப்பட்டுள்ளது . அவனுடைய இரண்டு வேட்டை நாய்களும் புலியோடு போராடுகின்றன. அணிகலன்கள் அனிந்து காணப்படுகிறது. இவன் புலியோடு போராடி புலியை கொன்று தானும் இறந்ததால் இந்த நடுகல் எடுக்கப்பட்டுள்ளது . இவரின் இரண்டு மனைவியரும் இவனோடு உடன்கட்டை ஏறியுள்ளனர் . ஆகவே இதை புலிக்குத்திப்பட்டான் கல் எனவும் சதிக்கல் எனவும் அழைக்கலாம். பகுதியின் வளத்தை குறிக்கும் வகையில் கிளிகள் காட்டப்பட்டுள்ளன",
+        image: "/images/HeroStones/1.jpg",
       },
       {
         title: "சின்னங்கள் மற்றும் கல்வெட்டுகள்",
-        description: "இந்த நடுகற்களில் காணப்படும் சின்னங்கள் மற்றும் எழுத்துக்களை ஆராய்கிறது...",
-        image: "/images/hero2.jpg",
+        description: "பெண் எப்போதும் தியாகத்தின் அடையாளம் என்பதை இந்த நடுகல் நமக்கு உணர்த்துகிறது. குந்தாணி ராஜ்ஜியத்தில் உள்ள நடுகல். இது ஒரு சிறு நடுகல்லில் கல்வெட்டுடன் காணப்படுகிறது. அதில்  தீத்தமலை உடனே தலை வெட்டு என குறிப்பிடப்படுகிறது. தலைவியின் தோற்றத்தில் இருக்கும் இவள் ஏதோ ஒரு நல்ல காரியத்துக்கு தன்னை பலி கொடுத்துள்ளாள். இதன் பின்னர் இதே தோற்றத்தில் பெரிய நடுகல் செய்து இன்றளவும் சாக்கியம்மாள் என வழிபட்டு வருகிறார்கள். இது 400 வருட பழையானது.",
+        image: "/images/HeroStones/2.jpg",
       },
       {
         title: "நடுகற்களின் வரலாற்று சூழல்",
-        description: "நடுகற்கள் வரலாற்றின் கதைகளில் பெற்றுள்ள பங்கு...",
-        image: "/images/hero3.jpg",
+        description: "நாம் தான் எறுதழுவலுக்கு சொந்தக்காரர்கள் ஆம் தென்பெண்ணை ஆற்றங்கரையோரம் அகரம் என்ற ஊரின் தென்னந்தோப்பில் இந்த அரியவகை ஏறுதழுவும் நடுகல் நம் ஆய்வுக்குழுவால் கண்டறியப்பட்டுள்ளது. இந்நடுகல் சுமார் 300 ஆண்டுகள் பழமையானதாகும். காளையானது பக்கவாட்டில் முன்னங்காலை தூக்கி ஓடுவது போல் காட்டப்பட்டுள்ளது. அதற்கு பின்னால் அக்காளையை அடக்க முற்படும் வீரன் காளையின் திமிலை இறுகப் பற்றிக்கொண்டு காளையின் முன்னங்காலில் தனது கால்களை பிண்ணிக்கொண்டு தொங்குகிறான். இதனால் காளையின் நாக்கு வாய்க்கு வெளியே தொங்குகிறது. ஏறு தழுவுதல் என்னும் சொல்லுக்கு ஏற்ப இந்த நடுகல் சிற்பம் வடிக்கப்பட்டுள்ளது சிறப்பாகும்.",
+        image: "/images/HeroStones/3.jpg",
       },
       {
         title: "பொருள் மற்றும் கட்டமைப்பு",
-        description: "நடுகற்கள் கட்டிடத்தில் பயன்படுத்தப்பட்ட பொருட்கள்...",
-        image: "/images/hero4.jpg",
+        description: "வீரத்தின் அடையாளம் தொகரப்பள்ளி சானாரப்பன் கோவில் ஆம் அதியமானோடு போரிட்டு தம் ஊரைக் காத்த வீரர்களுக்க எடுக்கப்பட்ட நடுகல் தொகுப்பு. பேரகடை ஏகாம்பரன், கும்பேரகடை ,வேள்முத்தரையன் சேவகர் மணியன் ஆகியோர் வீரமரணம் அடைந்துள்ளதாக இங்குள்ள கல்வெட்டுகள் தெரிவிக்கின்றன. போர் நடந்த மார்கழி 27 ல் இன்றும் நடுகல் வழிபாடு நடக்கிறது.",
+        image: "/images/HeroStones/4.jpg",
       },
       {
         title: "கலாச்சார முக்கியத்துவம்",
-        description: "நடுகற்களின் கலாச்சார முக்கியத்துவத்தை விளக்குகிறது...",
-        image: "/images/hero5.jpg",
+        description: "இசையால் வசமாகத இதையம் எதுவும் இல்லை. பெண்ணையாண்டவரையும், பக்தர்களையும் தம் இசையால் வசப்படுத்திய இந்த இரண்டு தம்பூரா இசைக்கும் பெண்களுக்கு நடுகல் எடுக்கப்பட்டு பெண்ணேஸ்வர மடத்தின் கோவில் வாசலுக்கு அருகே வைக்கப்பட்டுள்ளது.",
+        image: "/images/HeroStones/5.jpg",
       },
     ]
   },
@@ -96,10 +96,10 @@ export const departmentsData = {
       ]
     },
     ta: {
-      title: "நடுகற்கள்",
+      title: "பாறை ஓவியங்கள்",
       cards: [
         {
-          title: "நடுகற்களின் தோற்றம்",
+          title: "பாறை ஓவியங்கள்",
           description: "தென் இந்தியாவில் நடுகற்களின் ஆரம்ப வடிவங்களை ஆராய்கிறது...",
           image: "/images/hero1.jpg",
         },
