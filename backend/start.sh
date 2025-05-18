08bc790109345f24d1ec26750df581ca7c9f5331
ThiruchelvanT <91825720+ThiruchelvanT@users.noreply.github.com>
2025-05-18 12:05:15 +0530

    PathToRegex erro resolve

--- backend/start.sh
+++ backend/start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Clean cache only if needed
 if [ ! -d "node_modules" ]; then
   npm cache clean --force
 fi
 
-# Install dependencies
 npm install --production --legacy-peer-deps --no-audit
 
-# Start server
 node server.js
