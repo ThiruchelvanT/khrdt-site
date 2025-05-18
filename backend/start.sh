621bbd228f4d2f7b9ea2bf509336127e72a56c9a
ThiruchelvanTamilselvan <tthiruchelvansibi@gmail.com>
2025-05-18 10:25:41 +0530

    Fix path-to-regexp error and update routes

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 rm -rf node_modules
-npm install --production --legacy-peer-deps
-# Start the server
+rm -rf package-lock.json
+rm -rf ~/.npm/_logs/
+
+
+npm cache clean --force
+npm install --production --legacy-peer-deps --no-audit
+# Explicitly remove any nested path-to-regexp
+find node_modules -type d -name "path-to-regexp" -exec rm -rf {} +
+
+# Reinstall the correct version
+npm install path-to-regexp@6.2.1 --no-save
+
+# Start server
 node server.js
